de1b0f942d75267a30fdd05e2c40a8d48cb8d527
Andrew Kimpton <awk@onshape.com>
2018-10-10 17:01:18 -0400

    Update test script to reduce verbose output and just print a simple count of the documents fetched.

--- test/test.js
+++ test/test.js
@@ -2,15 +2,10 @@ var client = require('../index');
 
 describe('API Key Testing Spec', function () {
     describe('Use the API to make some get calls', function () {
-        it('should get the endpoints json', function (done) {
-            client(require('./apikey.js')).getEndpoints(function (data) {
-                console.log(data);
-                done();
-            });
-        });
         it('should get the user\'s documents', function (done) {
             client(require('./apikey.js')).getDocuments({}, function (data) {
-                console.log(data);
+                var response = JSON.parse(data);
+                console.log('Fetched ' + response.items.length + ' documents.');
                 done();
             });
         });
