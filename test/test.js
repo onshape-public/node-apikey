4b41ee27a8a205fccd5a61652d2bcb97356aa946
Andrew Kimpton <awk@onshape.com>
2018-10-10 15:01:30 -0400

    Update tests to load credentials from a local apikey.js file.

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ var client = require('../index');
 describe('API Key Testing Spec', function () {
     describe('Use the API to make some get calls', function () {
         it('should get the endpoints json', function (done) {
-            client(require('../example/getApiKeyFile')).getEndpoints(function (data) {
+            client(require('./apikey.js')).getEndpoints(function (data) {
                 console.log(data);
                 done();
             });
         });
         it('should get the user\'s documents', function (done) {
-            client(require('../example/getApiKeyFile')).getDocuments({}, function (data) {
+            client(require('./apikey.js')).getDocuments({}, function (data) {
                 console.log(data);
                 done();
             });
